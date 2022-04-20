3472c94df0b00953d9e71d752ec172f01ab175ca
Marina <lutientinuviel99@gmail.com>
2022-04-20 14:35:35 +1000

    Функциональный компонент

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# FlyNote study app
+
+Simple react project for view and create notes
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
 import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [notes, setNotes] = useState([]);
+    const [myNote, setMyNote] = useState('');
+
+    useEffect(
+        () => {
+            axios.get('https://bonus.dev.thewhite.ru/api/news-service/articles/list')
+                .then((response) => {
+                    console.log('get', response.data);
+                    setNotes(response.data);
+                })
+                .catch((error) => console.log(error));
+        },
+        [],
+    );
+
+    function saveNote() {
+        const newNotes = notes;
+        newNotes.push({
+            name: myNote,
+        });
+        console.log(newNotes);
+        setNotes(newNotes);
+    }
+
+    return (
+        <div className="App">
+        <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+                FlyNote
+            </p>
+        </header>
+        <main>
+            <h1>
+                FLYNOTE NOTES
+            </h1>
+            <main>
+                <div
+                    className="wrapper"
+                >
+                    <textarea
+                        placeholder="Напишите заметку"
+                        value={myNote}
+                        onChange={(event) => setMyNote(event.target.value)}
+                    />
+                    <button
+                        className="save-button"
+                        onClick={() => saveNote()}
+                    >
+                        Сохранить
+                    </button>
+                </div>
+                <ul
+                    className="notes"
+                >
+                    {
+                        notes.map((note, index) => (
+                            <li
+                                key={index}
+                            >
+                                {note.name}
+                            </li>
+                        ))
+                    }
+                </ul>
+            </main>
+        </main>
+        </div>
+    );
 }
 
 export default App;
